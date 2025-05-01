fd56053439608217160d87c740cfd8bbde210030
crL96 <190506813+crL96@users.noreply.github.com>
2025-05-01 14:09:54 +0200

    Set app port and view engine

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const path = require("node:path");
 const indexRouter = require("./routes/IndexRoute");
 const newRouter = require("./routes/newRoute");
 
 const app = express();
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.use("/", indexRouter);
 app.use("/new", newRouter);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
