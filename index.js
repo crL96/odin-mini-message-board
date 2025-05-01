ad982cd0cc63f346ead60c387f33f696b3d65116
crL96 <190506813+crL96@users.noreply.github.com>
2025-05-01 14:17:13 +0200

    Rename route

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("node:path");
 const indexRouter = require("./routes/IndexRoute");
-const newRouter = require("./routes/newRoute");
+const formRouter = require("./routes/formRoute");
 
 const app = express();
 
@@ -9,7 +9,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use("/", indexRouter);
-app.use("/new", newRouter);
+app.use("/new", formRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => {
