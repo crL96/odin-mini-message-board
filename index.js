e86c22f8df7d146a9509beac275ebb5a9e3d049b
crL96 <190506813+crL96@users.noreply.github.com>
2025-05-01 15:20:03 +0200

    Add css styling

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const indexRouter = require("./routes/IndexRoute");
 
 const app = express();
 
+//Handle static assets
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
