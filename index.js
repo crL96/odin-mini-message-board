41f6657a7385be57f55fdeaf56c39cec08623b05
crL96 <190506813+crL96@users.noreply.github.com>
2025-05-06 13:42:41 +0200

    Set port to environment var for deploy

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 //Router
 app.use("/", indexRouter);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log("Server running on port " + PORT);
 });
