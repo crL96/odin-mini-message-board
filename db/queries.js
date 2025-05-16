3324522d5b8e899be82037d5bbe1c6a2b02f18db
crL96 <190506813+crL96@users.noreply.github.com>
2025-05-16 09:43:18 +0200

    Cleanup date display in message (only shows date, now time or timezone) and sort by newest first

--- db/queries.js
+++ db/queries.js
@@ -1,7 +1,10 @@
 const pool = require("./pool");
 
 async function getAllMessages() {
-    const { rows } = await pool.query("SELECT * FROM messages");
+    const { rows } = await pool.query(`
+        SELECT * FROM messages
+        ORDER BY added DESC;
+        `);
     return rows;
 }
 
